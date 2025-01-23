e4191a9ecf907077927425c6fc42208065070d67
penguineo <subham020304@gmail.com>
2025-01-23 06:11:47 +0000

    demo: add userController.ts

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,23 @@
+import { User } from "../database/schema/userSchema";
+
+export const getUser = async () => {
+	try {
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const createUser = async () => {
+	try {
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const updateUser = async () => {
+	try {
+	} catch (err) {
+		console.log(err);
+	}
+};
+//etc
