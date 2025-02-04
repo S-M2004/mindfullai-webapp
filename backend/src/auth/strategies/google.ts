e7c08afc6ab00255f01a22308a5769def16b30d4
penguineo <subham020304@gmail.com>
2025-02-04 10:14:26 +0000

    feat: add google.ts
    - Added google.ts for authentication.

---
+++ backend/src/auth/strategies/google.ts
@@ -0,0 +1,39 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { User } from "../../database/schema/userSchema";
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.OAUTH_CLIENT_ID_GOOGLE,
+			clientSecret: process.env.OAUTH_CLIENT_SECRET_GOOGLE,
+			callbackURL: "/auth/google/redirect",
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			try {
+				let user = await User.findOne({ googleId: profile.id });
+
+				if (!user) {
+					user = new User({
+						googleId: profile.id,
+						email: profile.emails[0].value,
+						name: profile.displayName,
+					});
+
+					await user.save();
+				}
+
+				done(null, user);
+			} catch (err) {
+				done(err, null);
+			}
+		},
+	),
+);
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
